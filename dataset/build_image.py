23035b53b65835c313980e8b6662d52b9668e977
leoschn <schneider.leo@free.fr>
2026-01-14 12:36:47 +0100

    add error managment

--- dataset/build_image.py
+++ dataset/build_image.py
@@ -251,7 +251,12 @@ if __name__ == '__main__':
     print('building image')
     sample_list = glob.glob('/lustre/fswork/projects/rech/bun/ucg81ws/these/ms_diffusion/data/wiff_zeno/**.wiff', recursive=True)
     print(sample_list)
+    sample_with_error=[]
     for sample in sample_list:
         f_name = os.path.basename(sample).split('.wiff')[0]
         print(f_name)
-        data_out = build_image_ms2_wiff_2(f'//lustre/fswork/projects/rech/bun/ucg81ws/these/ms_diffusion/data/wiff_zeno/{f_name}.wiff',f'//lustre/fswork/projects/rech/bun/ucg81ws/these/ms_diffusion/data/image_zeno/{f_name}.pkl')
+        try :
+            data_out = build_image_ms2_wiff_2(f'//lustre/fswork/projects/rech/bun/ucg81ws/these/ms_diffusion/data/wiff_zeno/{f_name}.wiff',f'//lustre/fswork/projects/rech/bun/ucg81ws/these/ms_diffusion/data/image_zeno/{f_name}.pkl')
+        except:
+            sample_with_error.append(sample)
+    print('sample with error', sample_with_error)
