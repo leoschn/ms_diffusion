835006fd48ab6ad6d75fd720f34891225c22807b
leoschn <schneider.leo@free.fr>
2026-01-20 21:25:53 +0100

    add array job for build img

--- dataset/generate_args.py
+++ dataset/generate_args.py
@@ -5,7 +5,7 @@ sample_list = glob.glob('/lustre/fsn1/projects/rech/bun/ucg81ws/mzml/**.mzML', r
 i=0
 for sample in sample_list:
     f_name = os.path.basename(sample).split('.mzML')[0]
-    with open('../img_creation_dir/exec_{i}.txt'.format(i=i), 'w') as f:
+    with open('/lustre/fswork/projects/rech/bun/ucg81ws/these/ms_diffusion/img_creation_dir/exec_{i}.txt'.format(i=i), 'w') as f:
         f.write(f_name)
         i+=1
 print(i)
