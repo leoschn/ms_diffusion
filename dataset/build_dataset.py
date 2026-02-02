fc006cc163a139d737f3c8d71faa7913534e9b00
leoschn <schneider.leo@free.fr>
2026-02-02 15:50:36 +0100

    fix img pair build

--- dataset/build_dataset.py
+++ dataset/build_dataset.py
@@ -375,7 +375,7 @@ def main():
     sample_list = glob.glob('/lustre/fsn1/projects/rech/bun/ucg81ws/image/**.pkl', recursive=True)
     for f_name in sample_list:
         # if not os.path.exists(f'/lustre/fsn1/projects/rech/bun/ucg81ws/conditioning/conditioning_{sample_name}.pkl'):
-        sample_name = os.path.basename(f_name).split('.mzML')[0]
+        sample_name = os.path.basename(f_name).split('.pkl')[0]
         #     extract_detected_features_zeno_gaussian(out_path=f'/lustre/fsn1/projects/rech/bun/ucg81ws/conditioning/conditioning_{sample_name}.pkl',
         #                                             img_path=f'/lustre/fsn1/projects/rech/bun/ucg81ws/image/{sample_name}.pkl',
         #                                             diann_report=f'/lustre/fsn1/projects/rech/bun/ucg81ws/output/report_{sample_name}.tsv')
