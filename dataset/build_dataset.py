debc6589802cf5c15fab4f81a41fdb1a49163155
leoschn <schneider.leo@free.fr>
2026-02-02 16:31:00 +0100

    fix img pair build

--- dataset/build_dataset.py
+++ dataset/build_dataset.py
@@ -373,16 +373,20 @@ def main():
     #             sample_name=sample_name)
 
     sample_list = glob.glob('/lustre/fsn1/projects/rech/bun/ucg81ws/image/**.pkl', recursive=True)
+    i=0
     for f_name in sample_list:
         # if not os.path.exists(f'/lustre/fsn1/projects/rech/bun/ucg81ws/conditioning/conditioning_{sample_name}.pkl'):
         sample_name = os.path.basename(f_name).split('.pkl')[0]
         #     extract_detected_features_zeno_gaussian(out_path=f'/lustre/fsn1/projects/rech/bun/ucg81ws/conditioning/conditioning_{sample_name}.pkl',
         #                                             img_path=f'/lustre/fsn1/projects/rech/bun/ucg81ws/image/{sample_name}.pkl',
         #                                             diann_report=f'/lustre/fsn1/projects/rech/bun/ucg81ws/output/report_{sample_name}.tsv')
-        build_training_pairs(img_path=f'/lustre/fsn1/projects/rech/bun/ucg81ws/image/{sample_name}.pkl',
-                             cond_path=f'/lustre/fsn1/projects/rech/bun/ucg81ws/conditioning/conditioning_{sample_name}.pkl',
-                             out_dir='/lustre/fsn1/projects/rech/bun/ucg81ws/pairs/',
-                             sample_name=sample_name)
+        if os.path.exists('/lustre/fsn1/projects/rech/bun/ucg81ws/conditioning/conditioning_{}.pkl'.format(sample_name)):
+            i+=1
+            build_training_pairs(img_path=f'/lustre/fsn1/projects/rech/bun/ucg81ws/image/{sample_name}.pkl',
+                                 cond_path=f'/lustre/fsn1/projects/rech/bun/ucg81ws/conditioning/conditioning_{sample_name}.pkl',
+                                 out_dir='/lustre/fsn1/projects/rech/bun/ucg81ws/pairs/',
+                                 sample_name=sample_name)
+    print(i,' pairs created')
 
 
 if __name__ == '__main__':
