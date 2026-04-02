5606d7cdcd38bb6ddc2a5fbde41ef7577a8b25af
leoschn <schneider.leo@free.fr>
2026-04-02 10:00:20 +0200

    update img creation dir

--- dataset/build_dataset.py
+++ dataset/build_dataset.py
@@ -375,9 +375,9 @@ def main():
     sample_list = glob.glob('/lustre/fsn1/projects/rech/bun/ucg81ws/image/**.pkl', recursive=True)
     i=0
     for f_name in sample_list:
-
+        sample_name = os.path.basename(f_name).split('.pkl')[0]
         if not os.path.exists(f'/lustre/fsn1/projects/rech/bun/ucg81ws/conditioning/conditioning_{sample_name}.pkl'):
-            sample_name = os.path.basename(f_name).split('.pkl')[0]
+
             extract_detected_features_zeno_gaussian(out_path=f'/lustre/fsn1/projects/rech/bun/ucg81ws/conditioning/conditioning_{sample_name}.pkl',
                                                     img_path=f'/lustre/fsn1/projects/rech/bun/ucg81ws/image/{sample_name}.pkl',
                                                     diann_report=f'/lustre/fsn1/projects/rech/bun/ucg81ws/output/report_{sample_name}.tsv')
