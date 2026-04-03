f835707ce4d55b6d0ead9e54292ec3b4c4449544
leoschn <schneider.leo@free.fr>
2026-04-03 14:18:09 +0200

    update img creation dir

--- dataset/build_dataset.py
+++ dataset/build_dataset.py
@@ -377,17 +377,19 @@ def main():
     for f_name in sample_list:
         sample_name = os.path.basename(f_name).split('.pkl')[0]
         if not os.path.exists(f'/lustre/fsn1/projects/rech/bun/ucg81ws/conditioning/conditioning_{sample_name}.pkl'):
+            try :
+                extract_detected_features_zeno_gaussian(out_path=f'/lustre/fsn1/projects/rech/bun/ucg81ws/conditioning/conditioning_{sample_name}.pkl',
+                                                        img_path=f'/lustre/fsn1/projects/rech/bun/ucg81ws/image/{sample_name}.pkl',
+                                                        diann_report=f'/lustre/fsn1/projects/rech/bun/ucg81ws/output/report_{sample_name}.tsv')
 
-            extract_detected_features_zeno_gaussian(out_path=f'/lustre/fsn1/projects/rech/bun/ucg81ws/conditioning/conditioning_{sample_name}.pkl',
-                                                    img_path=f'/lustre/fsn1/projects/rech/bun/ucg81ws/image/{sample_name}.pkl',
-                                                    diann_report=f'/lustre/fsn1/projects/rech/bun/ucg81ws/output/report_{sample_name}.tsv')
-
-            if os.path.exists('/lustre/fsn1/projects/rech/bun/ucg81ws/conditioning/conditioning_{}.pkl'.format(sample_name)):
-                i+=1
-                build_training_pairs(img_path=f'/lustre/fsn1/projects/rech/bun/ucg81ws/image/{sample_name}.pkl',
-                                     cond_path=f'/lustre/fsn1/projects/rech/bun/ucg81ws/conditioning/conditioning_{sample_name}.pkl',
-                                     out_dir='/lustre/fsn1/projects/rech/bun/ucg81ws/pairs/',
-                                     sample_name=sample_name)
+                if os.path.exists('/lustre/fsn1/projects/rech/bun/ucg81ws/conditioning/conditioning_{}.pkl'.format(sample_name)):
+                    i+=1
+                    build_training_pairs(img_path=f'/lustre/fsn1/projects/rech/bun/ucg81ws/image/{sample_name}.pkl',
+                                         cond_path=f'/lustre/fsn1/projects/rech/bun/ucg81ws/conditioning/conditioning_{sample_name}.pkl',
+                                         out_dir='/lustre/fsn1/projects/rech/bun/ucg81ws/pairs/',
+                                         sample_name=sample_name)
+            except:
+                pass
     print(i,' pairs created')
 
 
