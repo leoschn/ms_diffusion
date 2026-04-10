cb80a3ebfb4137950b7effd4f3077eb1dbd109b7
leoschn <schneider.leo@free.fr>
2026-04-10 12:59:23 +0200

    add ddim difussion + remove useless code

--- Diffusion/train_ms.py
+++ Diffusion/train_ms.py
@@ -123,7 +123,7 @@ def train_ms(modelConfig: Dict):
         trainer = Diffusion.diffusion_ms_dyn.GaussianDiffusionTrainer_ms(
             net_model, modelConfig["beta_1"], modelConfig["beta_T"], modelConfig["T"],modelConfig["loss"]).to(device)
         sampler = Diffusion.diffusion_ms_dyn.GaussianDiffusionSampler_ms(
-            net_model, modelConfig["beta_1"], modelConfig["beta_T"], modelConfig["T"]).to(device)
+            net_model, modelConfig["beta_1"], modelConfig["beta_T"], modelConfig["T"],modelConfig["num_threshold"]).to(device)
 
     elif modelConfig['type']=='ddim':
         trainer = Diffusion.diffusion_DDIM.GaussianDiffusionTrainer_ms(
