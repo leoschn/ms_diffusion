9cb37a71e78062a5e40b648e8221887b1176f353
leoschn <schneider.leo@free.fr>
2026-04-23 16:05:48 +0200

    change reconstruct_image.py method

--- reconstruct_image.py
+++ reconstruct_image.py
@@ -4,29 +4,7 @@ import os
 import numpy as np
 import cv2
 
-def inverse_transform_np(img,crop_top=90,crop_left=0,crop_h=422,crop_w=1024,original_shape=(512, 1024),mean=3.04,
-                         std=3.04,final_size = (663, 901)):
-    """
-    img: numpy array (H, W)
-    """
-
-    # --- unnormalize ---
-    img = img * std + mean
-
-    # --- resize back to cropped size ---
-    img = cv2.resize(img, (crop_w, crop_h))  # (W, H)
-
-    # --- pad zeros to restore original shape ---
-    H0, W0 = original_shape
-    out = np.zeros((H0, W0), dtype=img.dtype)
-
-    out[crop_top:crop_top+crop_h, crop_left:crop_left+crop_w] = img
-
-    out = cv2.resize(out, (final_size[0], final_size[1]))
-    return out
-
-
-def reconstruct_image(base_name ,meta_data, base_size=(663,901),out_dir=None):
+def reconstruct_image(base_name ,meta_data,out_dir=None):
     if not(os.path.exists(out_dir)):
         os.mkdir(out_dir)
     list_img = []
@@ -34,7 +12,6 @@ def reconstruct_image(base_name ,meta_data, base_size=(663,901),out_dir=None):
     for i in range (n_wind):
         with open(base_name.replace('WIND_INDEX',str(i)), 'rb') as f:
             img =  pickle.load(f).squeeze()
-            img = inverse_transform_np(img,final_size=base_size)
         list_img.append(img)
     data_out = {'image': list_img, 'metadata': meta_data}
     out_path = base_name.replace('WIND_INDEX','full')
@@ -48,7 +25,6 @@ if __name__ == '__main__':
     base_name_list = [filename.rsplit('_', 2)[0] for filename in full_name]
     base_name_list = list(set(base_name_list))
     common_meta_data = pickle.load(open(DUMMY_IMAGE_PATH, 'rb'))['metadata']
-    final_size = pickle.load(open(DUMMY_IMAGE_PATH, 'rb'))['image'][0].shape
     for name in base_name_list:
-        reconstruct_image(name + '_WIND_INDEX_9.pkl', meta_data=common_meta_data, base_size=final_size,
+        reconstruct_image(name + '_WIND_INDEX_9.pkl', meta_data=common_meta_data,
                           out_dir='/lustre/fswork/projects/rech/bun/ucg81ws/these/ms_diffusion/sampled_full_reconstructed')
