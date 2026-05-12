6b97f0c150e7ae9b18dec000933747407e9b9f1d
leoschn <schneider.leo@free.fr>
2026-05-12 11:23:38 +0200

    fix metrics tracking

--- Diffusion/train_ms.py
+++ Diffusion/train_ms.py
@@ -19,12 +19,12 @@ from scheduler import GradualWarmupScheduler
 
 
 def train_ms(modelConfig: Dict):
-    import multiprocessing as mp
-    mp.set_start_method("spawn", force=True)
 
     rank = int(os.environ["SLURM_PROCID"])
     world_size = int(os.environ["SLURM_NTASKS"])
     local_rank = int(os.environ["SLURM_LOCALID"])
+
+    print(rank, local_rank, world_size)
 
     os.environ["RANK"] = str(rank)
     os.environ["WORLD_SIZE"] = str(world_size)
@@ -86,7 +86,7 @@ def train_ms(modelConfig: Dict):
                                             window_embedding=modelConfig["window_embd"],
                                             n_window=modelConfig["n_window"]).to(device)
     else:
-        raise 'model type not found'
+        raise ValueError("model type not found")
     net_model.apply(lambda m: setattr(m, 'weight', m.weight.contiguous())
     if hasattr(m, 'weight') else None)
     net_model = torch.nn.parallel.DistributedDataParallel(net_model,device_ids=[local_rank],
@@ -148,30 +148,31 @@ def train_ms(modelConfig: Dict):
         raise NotImplementedError
 
     # Sampled from standard normal distribution
-    mse_loss_fct = torch.nn.MSELoss()
+    mse_loss_fct = torch.nn.MSELoss(reduction='None')
 
     # start training
     best_epoch = 0
     best_loss = np.inf
     for e in range(modelConfig["epoch"]):
+        net_model.train()
         total_loss = 0
+        n_image=0
         sampler_train.set_epoch(e)
         if rank == 0:
-
-            i = 0
             pbar = tqdm(dataloader_train, dynamic_ncols=True)
         else :
             pbar = dataloader_train
         for images, cond, _, wind in pbar:
-
             # train
+            batch_size = images.size(0)
+            n_image += batch_size
             optimizer.zero_grad(set_to_none=True)
 
             cond = cond.to(device, non_blocking=True)
             x_0 = images.to(device, non_blocking=True)
             wind = wind.to(device, non_blocking=True)
 
-            loss = trainer(x_0, cond, wind).sum()
+            loss = trainer(x_0, cond, wind).mean()
 
             loss.backward()
             torch.nn.utils.clip_grad_norm_(
@@ -179,18 +180,26 @@ def train_ms(modelConfig: Dict):
             )
             optimizer.step()
 
+            loss_detached = loss.detach()
+
+            torch.distributed.all_reduce(
+                loss_detached,
+                op=torch.distributed.ReduceOp.SUM
+            )
+
+            loss_detached /= world_size
+
             if rank == 0:
-                i += 1
-                total_loss+=loss.item()
+                total_loss+=loss_detached.item()*batch_size
                 lr = warmUpScheduler.get_lr()[0]
                 pbar.set_postfix(ordered_dict={
                     "epoch": e,
-                    "loss: ": total_loss/i,
+                    "loss: ": total_loss,
                     "img shape: ": x_0.size(),
                     "LR": lr
                 })
         if rank == 0:
-            run.log({"epoch":e,"loss": total_loss/i,"LR": lr})
+            run.log({"epoch":e,"loss": total_loss/n_image,"LR": lr})
         warmUpScheduler.step()
 
         if rank == 0:
@@ -205,6 +214,7 @@ def train_ms(modelConfig: Dict):
             sampler_val.set_epoch(e)
 
             with torch.no_grad():
+                total_loss=0
                 n_image = 0
                 if rank == 0:
                     pbar_val =  tqdm(dataloader_val, dynamic_ncols=True)
@@ -215,18 +225,27 @@ def train_ms(modelConfig: Dict):
                 for images, cond ,path ,wind in pbar_val:
                     batch_size = images.size(0)
                     n_image+=batch_size
-
                     cond = cond.to(device, non_blocking=True)
                     images = images.to(device, non_blocking=True)
                     wind = wind.int().to(device, non_blocking=True)
                     noisyImage = torch.randn_like(images[:, :1, :, :])
                     sampledImgs = sampler(noisyImage, cond, wind)
-                    mse_loss = mse_loss_fct(sampledImgs, images)
+                    mse_loss = mse_loss_fct(sampledImgs, images).mean()
                     psnr_loss = 10 * torch.log10(1 / mse_loss)
 
 
-                    total_mse += mse_loss.item()*batch_size
-                    total_psnr += psnr_loss.item()*batch_size
+                    total_mse += mse_loss.item() *batch_size
+                    total_psnr += psnr_loss.item() *batch_size
+
+
+                    if rank == 0:
+                        lr = warmUpScheduler.get_lr()[0]
+                        pbar_val.set_postfix(ordered_dict={
+                            "epoch": e,
+                            "loss: ": total_mse/n_image,
+                            "img shape: ": images.size(),
+                            "LR": lr
+                        })
 
                     if rank == 0:
                         os.makedirs(modelConfig["sampled_dir_val"], exist_ok=True)
@@ -312,7 +331,7 @@ def train_ms(modelConfig: Dict):
             wind = wind.int().to(device, non_blocking=True)
             noisyImage = torch.randn_like(images[:, :1, :, :])
             sampledImgs = sampler(noisyImage, cond, wind)
-            mse_loss = mse_loss_fct(sampledImgs, images)
+            mse_loss = mse_loss_fct(sampledImgs, images).mean()
             psnr_loss = 10 * torch.log10(1 / mse_loss)
 
             total_mse += mse_loss.item() * batch_size
